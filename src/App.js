5226ff82625be5ba6c5b09f59827b35e57f96b9d
Yassir <elmalloukiyasser865@gmail.com>
2024-12-06 18:08:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [pages, setPages] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/all/`)
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.error(error));
+  }, [pages]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Flags</h1>
+      <ul>
+        {todos.map((todo, i) => (
+          <ListFlag todo={todo} key={i} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function ListFlag({ todo }) {
+  return (
+    <li>
+      <div className="flag-info">
+        <img src={todo.flags.svg} alt={todo.name.common} />
+        <h3>{todo.name.common}</h3>
+        <span>Region: {todo.region}</span>
+        <span>Capital: {todo.capital}</span>
+      </div>
+    </li>
+  );
+}
